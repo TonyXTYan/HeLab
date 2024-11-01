e9644ed12341f40984ea5ad3d881e9bbdc655612
Tony Yan <tony.yan.dev@gmail.com>
2024-11-02 02:20:40 +1100

    helab py version requirement fix

--- setup.py
+++ setup.py
@@ -32,5 +32,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.11',
+    python_requires='>=3.09',
+    # python_requires='>=3.11',
 )
