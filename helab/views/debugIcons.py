2e23617731244b06a17bbd3f705c4aa7aea28cbc
Tony XT Yan <tony.yan.dev@gmail.com>
2025-01-12 19:04:51 +1100

    AI generated many tests

--- helab/views/debugIcons.py
+++ helab/views/debugIcons.py
@@ -25,8 +25,8 @@ class DebugIconsWindow(QWidget):
         status_label = QLabel("Status Icons")
         status_layout.addWidget(status_label)
 
-        status_list = QListWidget()
-        status_list.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        self.status_list = QListWidget()
+        self.status_list.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         status_icons_dir = {**StatusIcons.ICONS_STATUS, **StatusIcons.ICONS_EXTRA}.items()
         status_icons_dir2 = dir(StatusIcons.ICONS_STATUS)
         status_icons_dir_len = len(status_icons_dir)
@@ -41,11 +41,11 @@ class DebugIconsWindow(QWidget):
             item = QListWidgetItem()
             item.setText(text)
             item.setIcon(icon)
-            status_list.addItem(item)
+            self.status_list.addItem(item)
             
 
 
-        status_layout.addWidget(status_list)
+        status_layout.addWidget(self.status_list)
         status_widget.setLayout(status_layout)
         splitter.addWidget(status_widget)
 
@@ -55,20 +55,20 @@ class DebugIconsWindow(QWidget):
         tool_label = QLabel("Tool Icons")
         tool_layout.addWidget(tool_label)
 
-        tool_list = QListWidget()
-        tool_list.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-        tool_list_dir = dir(ToolIcons)
-        tool_list_dir_len = 0
-        for attr in tool_list_dir:
+        self.tool_list = QListWidget()
+        self.tool_list.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        self.tool_list_dir = dir(ToolIcons)
+        self.tool_list_dir_len = 0
+        for attr in self.tool_list_dir:
             if attr.startswith('ICON_'):
-                tool_list_dir_len += 1
+                self.tool_list_dir_len += 1
                 icon = getattr(ToolIcons, attr)
                 if isinstance(icon, QIcon):
                     item = QListWidgetItem()
                     item.setText(attr)
                     item.setIcon(icon)
-                    tool_list.addItem(item)
-        tool_layout.addWidget(tool_list)
+                    self.tool_list.addItem(item)
+        tool_layout.addWidget(self.tool_list)
         tool_widget.setLayout(tool_layout)
         splitter.addWidget(tool_widget)
 
@@ -78,39 +78,39 @@ class DebugIconsWindow(QWidget):
         percentage_label = QLabel("Percentage Icons (DO NOT USE)")
         percentage_layout.addWidget(percentage_label)
 
-        percentage_list = QListWidget()
-        percentage_list.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-        # percentage_list_dir = dir(PercentageIcon)
-        # percentage_list_dir_len = 0
-        # for attr in percentage_list_dir:
+        self.percentage_list = QListWidget()
+        self.percentage_list.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        # self.percentage_list_dir = dir(PercentageIcon)
+        # self.percentage_list_dir_len = 0
+        # for attr in self.percentage_list_dir:
         #     if attr.startswith('ICON_'):
-        #         percentage_list_dir_len += 1
+        #         self.percentage_list_dir_len += 1
         #         icon = getattr(PercentageIcon, attr)
         #         if isinstance(icon, QIcon):
         #             item = QListWidgetItem()
         #             item.setText(attr)
         #             item.setIcon(icon)
-        #             percentage_list.addItem(item)
+        #             self.percentage_list.addItem(item)
         for div, qicon in PercentageIcon.ICONS.items():
             item = QListWidgetItem()
             item.setText(f"divID = {div} = {round(div * (360 / PercentageIcon._DIVS_COARSE))}deg ="
                          f" {round(div * (100 / PercentageIcon._DIVS_COARSE))}%")
             item.setIcon(qicon)
-            percentage_list.addItem(item)
-        percentage_list_dir_len = len(PercentageIcon.ICONS)
-        percentage_layout.addWidget(percentage_list)
+            self.percentage_list.addItem(item)
+        self.percentage_list_dir_len = len(PercentageIcon.ICONS)
+        percentage_layout.addWidget(self.percentage_list)
         percentage_widget.setLayout(percentage_layout)
         splitter.addWidget(percentage_widget)
 
-        # layout.setStretchFactor(status_list, status_icons_dir_len)
-        # layout.setStretchFactor(tool_list, tool_list_dir_len)
-        # layout.setStretchFactor(percentage_label, percentage_list_dir_len)
+        # layout.setStretchFactor(self.status_list, status_icons_dir_len)
+        # layout.setStretchFactor(self.tool_list, self.tool_list_dir_len)
+        # layout.setStretchFactor(percentage_label, self.percentage_list_dir_len)
         #
-        status_list.setMinimumHeight(200)
-        tool_list.setMinimumHeight(200)
-        percentage_list.setMinimumHeight(200)
+        self.status_list.setMinimumHeight(200)
+        self.tool_list.setMinimumHeight(200)
+        self.percentage_list.setMinimumHeight(200)
 
-        logging.debug(f"{len(status_icons_dir)} status icons, {tool_list_dir_len} tool icons, {percentage_list_dir_len} percentage icons")
+        logging.debug(f"{len(status_icons_dir)} status icons, {self.tool_list_dir_len} tool icons, {self.percentage_list_dir_len} percentage icons")
 
         layout.addWidget(splitter)
         self.setLayout(layout)
